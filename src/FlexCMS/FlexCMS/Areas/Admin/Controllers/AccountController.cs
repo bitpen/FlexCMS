e6e285f6c70502afcfb7ce5f18adeddb4ef8e401
Jonathan <bitpen@gmail.com>
2015-04-22 14:48:22 -0400

    Change Password
    View/Controller for change password functionality for current
    authenticated user.

--- src/FlexCMS/FlexCMS/Areas/Admin/Controllers/AccountController.cs
+++ src/FlexCMS/FlexCMS/Areas/Admin/Controllers/AccountController.cs
@@ -14,6 +14,41 @@ namespace FlexCMS.Areas.Admin.Controllers
     public class AccountController : Controller
     {
         /// <summary>
+        /// Update the password for the current account
+        /// </summary>
+        /// <returns></returns>
+        [Authorize]
+        public ActionResult ChangePassword()
+        {
+            return View();
+        }
+
+        /// <summary>
+        /// Process a change password request for the current account
+        /// </summary>
+        /// <param name="oldPassword"></param>
+        /// <param name="newPassword"></param>
+        /// <returns></returns>
+        [Authorize]
+        [HttpPost]
+        public ActionResult ChangePassword(string oldPassword, string newPassword)
+        {
+            if (!User.Identity.IsAuthenticated)
+            {
+                return RedirectToAction("LogOn");
+            }
+
+            var username = User.Identity.Name;
+            var auth = new SQLUserPassAuth();
+            if (auth.ChangePassword(username, null, oldPassword, newPassword))
+            {
+                return RedirectToAction("Settings");
+            }
+           
+            return View();
+        }
+
+        /// <summary>
         /// Sign out of the admin portion of the site
         /// </summary>
         /// <returns></returns>
@@ -46,11 +81,22 @@ namespace FlexCMS.Areas.Admin.Controllers
                 return RedirectToAction("Index", "Home");
             }
             
-
             return View();
         }
 
+        /// <summary>
+        /// Main landing page for individual account settings
+        /// </summary>
+        /// <returns></returns>
+        [Authorize]
+        public ActionResult Settings()
+        {
+            if (!User.Identity.IsAuthenticated)
+            {
+                return RedirectToAction("LogOn");
+            }
 
-
+            return View();
+        }
     }
 }
